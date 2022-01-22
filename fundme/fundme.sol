7a5098dea93be627b9ad72116ddc4882a80b6ede
Enda Kelly <kelly.enda@gmail.com>
2022-01-22 13:07:59 +0000

    added: chainlink price feed via ABI (application binary interface)

--- fundme/fundme.sol
+++ fundme/fundme.sol
@@ -2,11 +2,22 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
+// Get the latest ETH/USD price from chainlink price feed
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
 contract FundMe {
 
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
         addressToAmountFunded[msg.sender] += msg.value;
+        // need to know ETH -> USD conversion rate
      }
+
+    //function to get the version of the chainlink pricefeed
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
 }
