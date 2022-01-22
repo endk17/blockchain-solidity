242fa1ebee4908c9cb62bfe498bff86498093ba8
Enda Kelly <kelly.enda@gmail.com>
2022-01-22 13:25:27 +0000

    added: eth / usd price + unit256 convert

--- fundme/fundme.sol
+++ fundme/fundme.sol
@@ -20,4 +20,11 @@ contract FundMe {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
     }
+
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+         // ETH/USD rate in 18 digit 
+         return uint256(answer * 10000000000);
+    }
 }
